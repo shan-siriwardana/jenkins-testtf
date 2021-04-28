7293cb49a2ad531a7317d7f84f339929bfa96b99
shan-siriwardana <44491923+shan-siriwardana@users.noreply.github.com>
2021-04-28 12:10:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "3.37.0"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     aws = {
+#       source = "hashicorp/aws"
+#       version = "3.37.0"
+#     }
+#   }
+# }
 
 provider "aws" {
   region = "us-east-2"
